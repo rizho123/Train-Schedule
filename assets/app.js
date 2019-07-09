e6727e6f2d89efd190f64005155e63ca7bf01c8f
rizho123 <v-riczho@microsoft.com>
2019-07-08 23:28:52 -0700

    updating files

---
+++ assets/app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBMOUz9b6svst9I2x1pr1hft1KH2el7YpM",
+    authDomain: "project1-8ad64.firebaseapp.com",
+    databaseURL: "https://project1-8ad64.firebaseio.com",
+    projectId: "project1-8ad64",
+    storageBucket: "",
+    messagingSenderId: "383237108659",
+    appId: "1:383237108659:web:b20764e6d4aa1bfa"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
